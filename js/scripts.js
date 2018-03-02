dc2aaf968e42dbafdd7f5263d3ee68141f718894
Alexander Karari <karari.alexander@gmail.com>
2018-03-02 15:54:17 +0300

    User Interface
    User Interface added and working

--- js/scripts.js
+++ js/scripts.js
@@ -28,11 +28,9 @@ Contender.prototype.rollone = function() {
 Contender.prototype.hold = function () {
   this.totalscore += this.temporaryscore;
   this.temporaryscore = 0;
-  // this.changeturn();
   alert(this.participantName + ", your turn is over, SWITCH UP!");
 }
 
-// // changing turn
 Contender.prototype.winnerCheck = function () {
   if (this.totalscore >= 100) {
     alert(this.participantName + " HURRAY! YOU ARE THE WINNER!!");
@@ -50,3 +48,71 @@ var clearValues = function(){
   $(".firstPlayerName").val("");
   $(".secondPlayerName").val("");
 }
+
+
+// User Interface
+$(document).ready(function() {
+
+  $("button#start").click(function(event){
+    player1 = new Contender(true);
+    player2 =  new Contender(false);
+    $(".player-console").show();
+    $(".start-menu").hide();
+
+    var firstPlayerName = $(".firstPlayerName").val();
+    $("#firstPlayerName").text(firstPlayerName);
+
+    var secondPlayerName = $(".secondPlayerName").val();
+    $("#secondPlayerName").text(secondPlayerName);
+
+    player1.participantName=firstPlayerName;
+    player2.participantName=secondPlayerName;
+
+  });
+
+  $("button#new-game").click(function(event){
+    $(".player-console").hide();
+    clearValues();
+    player1.newGame();
+    player2.newGame();
+    $("#round-total-1").empty();
+    $("#total-score-1").empty();
+    $("#die-roll-1").empty();
+    $("#round-total-2").empty();
+    $("#total-score-2").empty();
+    $("#die-roll-2").empty();
+
+    $(".start-menu").show();
+  });
+
+  $("button#player1-roll").click(function(event){
+    player1.roll = tossdice();
+    $("#die-roll-1").text(player1.roll);
+    player1.rollone();
+    $("#round-total-1").text(player1.temporaryscore);
+  });
+
+  $("button#player2-roll").click(function(event){
+    player2.roll = tossdice();
+    $("#die-roll-2").text(player2.roll);
+    player2.rollone();
+    $("#round-total-2").text(player2.temporaryscore);
+  });
+
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    player1.winnerCheck();
+  });
+
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
+  });
+
+});
